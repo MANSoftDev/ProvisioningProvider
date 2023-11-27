1481c1a1157d9f0ec2d4702deb23ac6210bea835
Mark N <mark@mansoftdev.com>
2023-11-27 07:05:31 -0700

    Add files via upload

---
+++ CPSiteProvider/Features/SiteData/SiteData.feature
@@ -0,0 +1,7 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="a4c6c58b-197a-42ef-b242-0068d943165b" isHidden="true" featureId="a4c6c58b-197a-42ef-b242-0068d943165b" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="CPSiteProvider Provisioning Data" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="c3a914df-2da2-492b-b730-717e0feb469b" />
+    <projectItemReference itemId="07ca0744-394f-4e6c-984b-a15ae6a35d7c" />
+  </projectItems>
+</feature>
---
+++ CPSiteProvider/ProvisioningProvider.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using System.IO;
+using System.Xml.Linq;
+
+namespace CPSiteProvider
+{
+    public class ProvisioningProvider : SPWebProvisioningProvider
+    {
+        private const string SITE_TEMPLATE = "STS#1";
+
+        public override void Provision(SPWebProvisioningProperties props)
+        {
+            // Create a blank site to begin from
+            props.Web.ApplyWebTemplate(SITE_TEMPLATE);
+
+            // Save this so it is available in other methods
+            Properties = props;
+
+            SPSecurity.CodeToRunElevated code = new SPSecurity.CodeToRunElevated(CreateSite);
+            SPSecurity.RunWithElevatedPrivileges(code);
+        }
+
+        #region Private Methods
+
+        /// <summary>
+        /// Create the site
+        /// </summary>
+        private void CreateSite()
+        {
+            using(SPSite site = new SPSite(Properties.Web.Site.ID))
+            {
+                using(SPWeb web = site.OpenWeb(Properties.Web.ID))
+                {
+                    // Add specified features to this site
+                    AddSiteFeatures(site);
+
+                    // Add specified features to this web
+                    AddWebFeatures(web);
+
+                    // Add new default page
+                    AddDefaultPage(web);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add features to the given site
+        /// </summary>
+        /// <param name="site">SPSite to add features to</param>
+        private void AddSiteFeatures(SPSite site)
+        {
+            List<XElement> features = (from f in DataFile.Elements("SiteFeatures")
+                                .Elements("Feature")
+                                 select f).ToList();
+
+            foreach(XElement feature in features)
+            {
+                // Make sure the feature hasn't already been activated
+                SPFeature f = site.Features[new Guid(feature.Attribute("ID").Value)];
+                if(f == null)
+                {
+                    site.Features.Add(new Guid(feature.Attribute("ID").Value));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add features to the given web
+        /// </summary>
+        /// <param name="web">SPWeb to add features to</param>
+        private void AddWebFeatures(SPWeb web)
+        {
+            List<XElement> features = (from f in DataFile.Elements("WebFeatures")
+                                .Elements("Feature")
+                                select f).ToList();
+
+            foreach(XElement feature in features)
+            {
+                // Make sure the feature hasn't already been activated
+                SPFeature f = web.Features[new Guid(feature.Attribute("ID").Value)];
+                if(f == null)
+                {
+                    web.Features.Add(new Guid(feature.Attribute("ID").Value));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add default page to site
+        /// </summary>
+        /// <param name="web">SPWeb to add page to</param>
+        private void AddDefaultPage(SPWeb web)
+        {
+            string file = (from f in DataFile.Elements("DefaultPage")
+                            select f).Single().Attribute("file").Value;
+
+            string filePath = FeaturePath + "\\" + file;
+            TextReader reader = new StreamReader(filePath);
+
+            MemoryStream outStream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(outStream);
+
+            writer.Write(reader.ReadToEnd());
+            writer.Flush();
+
+            web.Files.Add("Default.aspx", outStream, true);
+        }
+
+        #endregion
+
+        #region Properties
+
+        private SPWebProvisioningProperties Properties { get; set; }
+
+        private XElement DataFile
+        {
+            get
+            {
+                XElement featuresXml = null;
+                if(Properties != null)
+                {
+                    // Construct the path from the SharePoint root folder to
+                    // the file specified in the webtemp
+                    string path = SPUtility.GetGenericSetupPath(Path.GetDirectoryName(Properties.Data));
+                    path = Path.Combine(path, Path.GetFileName(Properties.Data));
+
+                    // Load the xml file
+                    featuresXml = XElement.Load(path);
+                }
+
+                return featuresXml;
+            }
+        }
+
+        private string FeaturePath
+        {
+            get
+            {
+                string path = string.Empty;
+                if(Properties != null)
+                {
+                    // Construct the path from the SharePoint root folder to
+                    // the file specified in the webtemp
+                    path = SPUtility.GetGenericSetupPath(Path.GetDirectoryName(Properties.Data));
+                    path = Path.GetDirectoryName(path);
+                }
+                return path;
+            }
+        }
+
+        #endregion
+    }
+}
